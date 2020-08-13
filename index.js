c6393102e60302315319be1e0b6e71367d6fce2d
Ameco <hamecow@gmail.com>
2020-08-13 13:18:59 -0700

    Add input generated questions

--- index.js
+++ index.js
@@ -5,10 +5,46 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer
     .prompt([{
-        type: 'input',
-        name: 'title',
-        message: 'What is your project title?'
-    }])
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation information:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information:'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please provide contributing information:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide testing information:'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please provide your github username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide your email:'
+        }
+    ])
     .then(answers => {
         const text = writeText(answers);
         return writeFileAsync(`${answers.title}.md`, text);
@@ -24,5 +60,27 @@ inquirer
 function writeText(answers) {
     return `
         # ${answers.title}
-   `;
+        ### ${answers.description}
+        ##Table of Contents
+            ###
+            ###
+            ###
+            ###
+            ###
+            ###
+        ##Installation
+        ${answers.installation}
+        ##Usage
+        ${answers.usage}
+        ##License
+        ##Contributing
+        ${answers.contributing}
+        ##Tests
+        ${answers.tests}
+        ##Questions
+        #### Github Profile
+        **[${answers.username}](https://github.com/${answers.username})**
+        #### Please submit questions to:
+        ***${answers.email}***
+        `
 }
